2c68cf83ae5e68bb14d8e661d4acb59c6838f65f
jEUGENEdev <genyamateyuk@gmail.com>
2024-05-19 00:32:33 +0300

    blocking main thread

--- ReportService/src/main/java/com/factory/reportservice/ReportServiceApplication.java
+++ ReportService/src/main/java/com/factory/reportservice/ReportServiceApplication.java
@@ -3,11 +3,18 @@ package com.factory.reportservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.concurrent.locks.ReentrantLock;
+
 @SpringBootApplication
 public class ReportServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ReportServiceApplication.class, args);
+
+		// blocking main thread
+		ReentrantLock lock = new ReentrantLock();
+		lock.lock();
+		lock.lock();
 	}
 
 }
