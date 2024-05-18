d7b3fc19c46500492637e39a19b5eb0e390b4318
JavaKira <Javakira@yandex.ru>
2024-05-18 09:15:47 +0300

    feat: adds OpenAPI (swagger) and config it for work

--- src/main/java/com/factory/SmartFinance/SmartFinanceApplication.java
+++ src/main/java/com/factory/SmartFinance/SmartFinanceApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SmartFinanceApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(SmartFinanceApplication.class, args);
 	}
-
 }
